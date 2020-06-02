5e3519979664b94e88f591137242f68586e38ef2
bonepeople <bonepeople@sina.com>
2020-06-02 10:11:50 +0800

    创建项目

---
+++ simple/src/main/java/com/bonepeople/android/visuallog/simple/App.java
@@ -0,0 +1,10 @@
+package com.bonepeople.android.visuallog.simple;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
